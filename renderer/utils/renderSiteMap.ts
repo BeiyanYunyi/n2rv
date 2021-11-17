49a56e057ca21159bdfc29f08b3879f3068dc1c9
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-11-17 15:34:49 +0800

    修

--- renderer/utils/renderSiteMap.ts
+++ renderer/utils/renderSiteMap.ts
@@ -3,10 +3,9 @@ import config from '../../config/config.json';
 import Storage from '../../src/instances/Storage';
 
 const renderSiteMap = async () => {
-  const port = process.env.PORT || 3000;
   const topics = await Storage.getAllTopics(0, 1000, false, false);
-  const urlAry = [`https://${config.servAddr}:${port}`].concat(
-    topics.map((topic) => `https://${config.servAddr}:${port}/topic/${topic.topicID}`),
+  const urlAry = [`https://${config.servAddr}:${config.listenPort}`].concat(
+    topics.map((topic) => `https://${config.servAddr}:${config.listenPort}/topic/${topic.topicID}`),
   );
   return urlAry.join('\n');
 };
