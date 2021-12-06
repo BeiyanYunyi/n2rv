c0a9e2c2635b6c51b045e32b9c1bc9400f337e0f
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-12-06 21:13:10 +0800

    修正自动聚焦的问题

--- pages/index/components/Editor.tsx
+++ pages/index/components/Editor.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@mui/material';
 import { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
 import Vditor from 'vditor';
+import AppSetStateAction from '../../../src/types/AppSetStateAction';
 
 export interface EditorRef {
   value: () => string | undefined;
@@ -9,21 +11,7 @@ export interface EditorRef {
   blur: () => void | undefined;
 }
 
-const Editor = forwardRef((_props, ref) => {
-  const [vd, setVd] = useState<Vditor>();
-
-  useImperativeHandle(ref, () => {
-    const value = () => vd?.getValue();
-    const html = () => vd?.getHTML();
-    const clearCache = () => {
-      vd?.setValue('');
-      vd?.clearCache();
-    };
-    const focus = () => vd?.focus();
-    const blur = () => vd?.blur();
-    return { value, html, clearCache, focus, blur };
-  });
-
+const AppVditor = ({ setVd }: { setVd: AppSetStateAction<Vditor | undefined> }) => {
   useEffect(() => {
     const vditor = new Vditor('vditor', {
       after: () => {
@@ -58,8 +46,37 @@ const Editor = forwardRef((_props, ref) => {
         },
       ],
     });
-  }, []);
-  return <div id="vditor" />;
+  }, [setVd]);
+  return <div style={{ height: window.innerHeight }} id="vditor" />;
+};
+
+const Editor = forwardRef((_props, ref) => {
+  const [vd, setVd] = useState<Vditor>();
+  const [open, setOpen] = useState(false);
+
+  useImperativeHandle(ref, () => {
+    const value = () => vd?.getValue();
+    const html = () => vd?.getHTML();
+    const clearCache = () => {
+      vd?.setValue('');
+      vd?.clearCache();
+    };
+    const focus = () => vd?.focus();
+    const blur = () => vd?.blur();
+    return { value, html, clearCache, focus, blur };
+  });
+
+  return (
+    <div>
+      {open ? (
+        <AppVditor setVd={setVd} />
+      ) : (
+        <Button fullWidth sx={{ height: window.innerHeight / 2 }} onClick={() => setOpen(true)}>
+          点击输入内容
+        </Button>
+      )}
+    </div>
+  );
 });
 
 export default Editor;
